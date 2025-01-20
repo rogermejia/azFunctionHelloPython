fed1270f6096f736d4ab232458d53f8e0ed59b5a
rogermejia <Roger.Alonso7412@gmail.com>
2025-01-19 18:18:51 -0600

    first push

---
+++ function_app.py
@@ -0,0 +1,25 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="pyGithubactions")
+def pyGithubactions(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "Pass a name in the query string",
+             status_code=200
+        )
